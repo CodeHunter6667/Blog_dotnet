f075ded9f4f93f18180c3d56ea3743d003a57e40
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-07-21 17:24:53 -0300

    feat: metodos Update e Delete

--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -25,6 +25,33 @@ public class UserRepository
     public User GetById(int id)
         => _connection.Get<User>(id);
     
-    public void Insert(User user)
-        => _connection.Insert(user);
+    public void Create(User user)
+    {
+        user.Id = 0;
+        _connection.Insert(user);
+    }
+
+    public void Update(User user)
+    {
+        if(user.Id != 0)
+        {
+            _connection.Update(user);
+        }
+    }
+
+    public void Delete(int id)
+    {
+        if (id != 0)
+            return;
+        var user = _connection.Get<User>(id);
+        _connection.Delete(user);
+    }
+
+    public void Delete(User user)
+    {
+        if(user.Id != 0)
+        {
+            _connection.Delete<User>(user);
+        }
+    }
 }
